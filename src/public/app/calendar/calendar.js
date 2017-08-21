e93b53b6ecb006408989861865115a60aae09844
marjob <marjoballabani@gmail.com>
2017-08-21 21:53:27 +0200

    feat: dumy pubnub subscribe and publish

--- src/public/app/calendar/calendar.js
+++ src/public/app/calendar/calendar.js
@@ -4,14 +4,52 @@ angular
     templateUrl: 'app/calendar/calendar.html',
     controller: function (
         DataFactory,
-        Pubnub
+        Pubnub,
+        $rootScope,
+        $scope
     ) {
         this.slots = DataFactory.slots
+
+        /**
+         * Get slot list
+         */
         DataFactory.getSlots((success) => {
-            console.log(success.data)
             this.slots = success.data
         }, (error) => {
             alert(error.data)
         })
+
+        /**
+         * Subscribe to channel
+         */
+        Pubnub.subscribe({
+            channels: ['Channel-egrothab8'],
+            triggerEvents: ['message']
+        })
+
+        /**
+         * Do something when you get a message
+         */
+        $rootScope.$on(Pubnub.getMessageEventNameFor('Channel-egrothab8'), function (ngEvent, envelope) {
+            $scope.$apply(function () {
+                console.log(envelope.message)
+            });
+        });
+
+        Pubnub.publish(
+            {
+                message: {
+                    text: 'Hello!'
+                },
+                channel: 'Channel-egrothab8'
+            },
+            function (status, response) {
+                if (status.error) {
+                    console.log(status)
+                } else {
+                    console.log("message Published")
+                }
+            }
+        );
     }
 });
