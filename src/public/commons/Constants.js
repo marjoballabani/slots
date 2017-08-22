ed237073f7b7dd851b19aee82e3b585e7636b6cb
marjo <mballabani@helius-software.com>
2017-08-22 09:55:29 +0200

    not part of task, deploying on heroku

--- src/public/commons/Constants.js
+++ src/public/commons/Constants.js
@@ -2,6 +2,6 @@ angular
 .module("commons")
 .constant('Constants', {
     Url: {
-        SLOTS: 'http://localhost:8081/jsons/slots.json'
+        SLOTS: 'https://calendar-slots.herokuapp.com/jsons/slots.json'
     }
 });
